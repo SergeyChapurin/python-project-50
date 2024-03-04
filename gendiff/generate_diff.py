373821c53cfa142f5ee6d5220a0526c726574a56
Sergey Chapurin <goodwin7@list.ru>
2024-03-04 20:43:25 +0400

    modified:   gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,7 +3,7 @@ from gendiff.build_diff import build_diff
 from gendiff.formatters.format_name import get_format_name
 
 
-def generate_diff(file1_path, file2_path, format_name):
+def generate_diff(file1_path, file2_path, format_name='stylish'):
     read_data1, file1_format = read_file(file1_path)
     read_data2, file2_format = read_file(file2_path)
 
