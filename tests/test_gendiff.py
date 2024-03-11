e9cff9d6d1748643ba65bb57c49ad2784756cce0
Sergey Chapurin <goodwin7@list.ru>
2024-03-11 17:13:34 +0400

    fixed linter errors

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,12 +7,14 @@ from gendiff import generate_diff
 def get_abspath(file):
     return os.path.join(f'{dirname(abspath(__file__))}', 'fixtures', f'{file}')
 
+
 test_data = [
     ('file1_nested.json', 'file2_nested.json', 'expected_res_stylish.txt', 'stylish'),
     ('file1_nested.yml', 'file2_nested.yml', 'expected_res_stylish.txt', 'stylish'),
     ('file1_nested.yml', 'file2_nested.yml', 'expected_res_plain.txt', 'plain'),
     ('file1_nested.json', 'file2_nested.json', 'expected_res_json.txt', 'json'),
 ]
+
 
 @pytest.mark.parametrize("file1, file2, expected_res, format_name", test_data)
 def test_generate_diff(file1, file2, expected_res, format_name):
